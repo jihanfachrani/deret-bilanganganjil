bfb99052f080b337868852b89bb7d0eee7f8be05
jihanfachrani <jihan.fachrani@unida.ac.id>
2021-12-19 09:02:45 +0800

    Deret bilangan ganjil
    Menampilkan deretan bilangan ganjil dengan tag program C++

---
+++ bilanganganjil.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	cout << " Menampilkan Bilangan Ganjil \n ";
+	cout <<"\n    ";
+	
+	
+		for (int ganjil = 1;  ganjil <=9;  ganjil++)
+		if ( ganjil % 2  == 1) {
+				cout << ganjil << " ";
+				
+		}
+}
+
